2af2be601b0280abf897b62bb79212da46880729
Gorin <v.peacedeath@gmail.com>
2022-03-20 19:44:21 +0300

    new files

---
+++ js/Do raboti so stranicami/12_tipizacia_JS.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// to string
+
+// 1)
+                // console.log(typeof(String(null)));
+
+// 2)
+                // console.log(typeof(5 + ''));
+
+                // const num = 5;
+                // console.log("http://vk.com/catalog/" + num);
+
+                // const fontSize = 26 + 'px';
+
+
+
+
+// To Number
+
+// 1)
+
+                 // console.log(typeof(Number('4')));
+
+// 2)
+                // console.log(typeof(+'5'));
+
+// 3)
+                // console.log(typeof(parseInt("15px", 10)));
+
+                // let answ = +prompt("Hello", "");
+
+
+// To Boolean
+
+// 0, '', null, undefined, NaN; - всегда ! false !
+
+// 1)
+
+                // let switcher = null;
+
+                // if (switcher) {
+                //     console.log('Working');
+                // } else {
+                //     console.log('Not Working');
+                // }
+
+                // switcher = 1;
+
+                // if (switcher) {
+                //     console.log('Working');
+                // }
+
+// 2)
+
+                //  console.log(typeof(Boolean('10')));
+                
+// 3)
+
+// console.log(typeof(!!'10')); - !! превращает значение в boolean (true/false)
